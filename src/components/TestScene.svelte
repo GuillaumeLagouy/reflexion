678c1dd87ee99f1274cacad00b0d4e385fbeb33c
GuillaumeLagouy <glagouy.pro@gmail.com>
2019-06-11 19:47:24 +0200

    Create frame with roughjs

--- src/components/TestScene.svelte
+++ src/components/TestScene.svelte
@@ -1,11 +1,10 @@
 <script >
-    import {Frame} from '../common/Frame';
-    const frame1 = new Frame(300, 450);
-    frame1.draw();
+    import { Frame } from '../common/Frame';
+
+    const frame1 = new Frame('frame1', 0, 0, 400, 400).draw();
 </script>
 
 <style>
 
 </style>
 
-<h1>Test scene</h1>
