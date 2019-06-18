d4f24aaa9ab37240ed3cb77d041a9e0ab3586a6e
GuillaumeLagouy <glagouy.pro@gmail.com>
2019-06-18 11:20:31 +0200

    add frames sequence

--- src/configs/testSceneConfig.js
+++ src/configs/testSceneConfig.js
@@ -1,71 +1,66 @@
 import TweenMax from 'gsap';
 import anchor from '../constants/anchor';
+import {frameNumber} from "../stores/frameStore";
 
 export default [
     {
         id: 'frame1',
-        x: 0,
-        y: 0,
-        width: 20,
-        height: 20,
-        anchor: anchor.topLeft,
+        x: 50,
+        y: 50,
+        width: 35,
+        height: 25,
+        anchor: anchor.center,
         callback: (id) => {
             const el = document.querySelector(`#${id}`);
             el.style.background = '#dcc0ff';
+
+            el.addEventListener('click', () => {
+                frameNumber.update(n => n + 1);
+                el.style.opacity = 0
+            });
         }
     },
 
     {
         id: 'frame2',
-        x: 0,
-        y: 100,
-        width: 20,
-        height: 10,
-        anchor: anchor.bottomLeft,
+        x: 95,
+        y: 95,
+        width: 35,
+        height: 35,
+        anchor: anchor.bottomRight,
         callback: (id) => {
             const el = document.querySelector(`#${id}`);
-            el.style.background = '#55e9ff';
+            el.style.background = '#ffdda1';
+            el.style.opacity = 0;
+
+            frameNumber.subscribe(value => {
+                value === 1 ? el.style.opacity = 1 : null;
+            });
+            el.addEventListener('click', ()=>{
+                frameNumber.update(n => n + 1);
+                el.style.opacity = 0;
+            })
         }
     },
 
     {
         id: 'frame3',
-        x: 100,
-        y: 0,
-        width: 40,
-        height: 20,
-        anchor: anchor.topRight,
-        callback: (id) => {
-            const el = document.querySelector(`#${id}`);
-            el.style.background = '#8eff75';
-        }
-    },
-
-    {
-        id: 'frame4',
-        x: 100,
-        y: 100,
-        width: 40,
-        height: 20,
-        anchor: anchor.bottomRight,
-        callback: (id) => {
-            const el = document.querySelector(`#${id}`);
-            el.style.background = '#8eff75';
-            TweenMax.to(el, 1, {rotation: 180})
-        }
-    },
-
-    {
-        id: 'frame5',
         x: 50,
         y: 50,
-        width: 40,
-        height: 20,
+        width: 52.5,
+        height: 37.5,
         anchor: anchor.center,
         callback: (id) => {
             const el = document.querySelector(`#${id}`);
-            el.style.background = '#8eff75';
-            TweenMax.to(el, 1, {rotation: 180})
+            el.style.background = '#8782ff';
+            el.style.opacity = 0;
+
+            frameNumber.subscribe(value => {
+                value === 2 ? el.style.opacity = 1 : null;
+            });
+            el.addEventListener('click', () => {
+                el.style.opacity = 0;
+            })
         }
     },
 ];
