afcebaa98c6eab3f872f5e72fc5663ace9eef130
GuillaumeLagouy <43404518+GuillaumeLagouy@users.noreply.github.com>
2019-06-21 16:27:23 +0200

    Setup all frames (#17)
    * playtime scene
    
    * lunch scene
    
    * :construction: class scene
    
    * :construction: home scene

---
+++ src/configs/scenes/classSceneConfig.js
@@ -0,0 +1,85 @@
+import anchor from "../../constants/anchor";
+import Title from "../../components/Title.svelte";
+
+export default [
+    {
+        id: 's6-bell',
+        x: 50,
+        y: 26,
+        anchor: anchor.center,
+        width: 30,
+        height: 30,
+    },
+
+    {
+        id: 's6-title',
+        x: 50,
+        y: 50,
+        anchor: anchor.center,
+        width: 30,
+        height: 10,
+        content: Title,
+        title: 'TITRE',
+        callback: id => {
+            const el = document.getElementById(id);
+            Object.assign(el.style, {
+                border: 0,
+                webkitBorderImage: 'none',
+            })
+        }
+    },
+
+    {
+        id: 's6-class',
+        x: 50,
+        y: 50,
+        anchor: anchor.center,
+        width: 50,
+        height: 50,
+    },
+
+    {
+        id: 'S6-math',
+        x: 100,
+        y: 0,
+        anchor: anchor.topRight,
+        width: 40,
+        height: 100,
+    },
+
+    {
+        id: 's6-noise',
+        x: 10,
+        y: 90,
+        anchor: anchor.bottomLeft,
+        width: 30,
+        height: 10,
+    },
+
+    {
+        id: 's6-alone',
+        x: 50,
+        y: 50,
+        anchor: anchor.center,
+        width: 10,
+        height: 20,
+    },
+
+    {
+        id: 's6-360',
+        x: 50,
+        y: 50,
+        anchor: anchor.center,
+        width: 100,
+        height: 100,
+    },
+
+    {
+        id: 's6-ink',
+        x: 50,
+        y: 50,
+        anchor: anchor.center,
+        width: 100,
+        height: 100,
+    },
+]
--- src/configs/scenesConfig.js
+++ src/configs/scenesConfig.js
@@ -2,9 +2,13 @@ import introSceneConfig from '../configs/scenes/introSceneConfig';
 import bathroomSceneConfig from '../configs/scenes/bathroomSceneConfig';
 import breakfastSceneConfig from '../configs/scenes/breakfastSceneConfig';
 import poolSceneConfig from './scenes/poolSceneConfig';
+import playtimeSceneConfig from './scenes/playtimeSceneConfig';
+import lunchSceneConfig from './scenes/lunchSceneConfig';
+import classSceneConfig from './scenes/classSceneConfig';
+import homeSceneConfig from './scenes/homeSceneConfig';
 
 export default [
-   {
+    {
         id: 'wakeup-scene',
         config: introSceneConfig,
         sequence: 0,
@@ -26,5 +30,47 @@ export default [
         id: 'swimming-pool-scene',
         config: poolSceneConfig,
         sequence: 3,
+    },
+
+    {
+        id: 'playtime-scene',
+        config: playtimeSceneConfig,
+        sequence: 4,
+        callback: id => {
+            const el = document.getElementById(id);
+            Object.assign(el.style, {
+                height: '200vh',
+            })
+        }
+    },
+
+    {
+        id: 'lunch-scene',
+        config: lunchSceneConfig,
+        sequence: 5,
+        callback: id => {
+            const el = document.getElementById(id);
+            Object.assign(el.style, {
+                height: '200vh',
+            })
+        }
+    },
+
+    {
+        id: 'class-scene',
+        config: classSceneConfig,
+        sequence: 6,
+        callback: id => {
+            const el = document.getElementById(id);
+            Object.assign(el.style, {
+                height: '200vh',
+            })
+        }
+    },
+
+    {
+        id: 'home-scene',
+        config: homeSceneConfig,
+        sequence: 7,
     }
 ]
